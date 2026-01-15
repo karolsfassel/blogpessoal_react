9988231c0b90750bd99f63275dfe3c849dedce86
Karol <karolfassel@gmail.com>
2026-01-15 14:24:13 -0300

    feat: adicionando rotas

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,9 +12,16 @@ function Footer(){
                 </p>
                 <p className='text-lg'> Acesse nossas redes sociais</p>
                 <div className='flex gap-2'>
-                    <LinkedinLogoIcon size={48} weight='bold'/>
-                    <InstagramLogoIcon size={48} weight='bold'/>
+                    <a href="https://www.linkedin.com/in/karolinesfdev/" target="_blank">
+                      <LinkedinLogoIcon size={48} weight='bold'/>
+                    </a>
+                    <a href="https://www.instagram.com/" target="_blank">
+                        <InstagramLogoIcon size={48} weight='bold'/>
+                    </a>
+
+                    <a href="https://www.facebook.com/" target="_blank">
                     <FacebookLogoIcon size={48} weight='bold'/>
+                    </a>
                 </div>
             </div>
         </div>

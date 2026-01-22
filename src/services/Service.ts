1f1e6476e8116b6003913974aa8c13f0c4e2d004
Karol <karolfassel@gmail.com>
2026-01-22 13:50:27 -0300

    Deploy do Projeto Projeto Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL:'https://blogpessoal-1-mn05.onrender.com'
+export const api = axios.create({
+   baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async(url:string, dados: object, setDados:Function) =>{
